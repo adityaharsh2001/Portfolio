033bceea970b0fad52c2ec2ab4870597e9688e6f
adityaharsh2001 <geekyadi118@gmail.com>
2022-01-17 15:42:41 +0530

    links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import {Container} from "./styles";
 
-import reactIcon from "../../assets/react-icon.svg";
 import linkedinIcon from "../../assets/linkedin.png";
 import githubIcon from "../../assets/github.png";
 import instagramIcon from "../../assets/instagram.png";
@@ -10,27 +9,25 @@ export function Footer(){
   return(
     <Container>
       <a href="/" className="logo">
-        <span>J</span>
-        <span>Tulio</span>
+        <span>A</span>
+        <span>H</span>
       </a>
-      <div>
-        <p>Este site foi feito com <img src={reactIcon}/> e muito ❤️</p>
-      </div>
+     
 
       <div className="social-media">
-        <a href="https://www.linkedin.com/in/jtsoares/" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/aditya-harsh-9b3559118" target="_blank" rel="noreferrer">
           <img src={linkedinIcon} alt="Linkedin" />
         </a>
 
-        <a href="https://github.com/joaotuliojt" target="_blank" rel="noreferrer">
+        <a href="https://github.com/adityaharsh2001" target="_blank" rel="noreferrer">
           <img src={githubIcon} alt="GitHub" />
         </a>
 
-        <a href="https://www.instagram.com/tulio_joaob/" target="_blank" rel="noreferrer">
+        <a href="https://www.instagram.com/geeky_adi/" target="_blank" rel="noreferrer">
           <img src={instagramIcon} alt="Instagram" />
         </a>
 
-        <a href="https://discord.com/users/426120432991862784" target="_blank" rel="noreferrer">
+        <a href="https://discord.com/users/607072384998178842" target="_blank" rel="noreferrer">
           <img src={discordIcon} alt="Discord" />
         </a>
       </div>
